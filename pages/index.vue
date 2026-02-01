9e950863e6f871ac0d31843914bd470946692ef8
Depreca1ed <70801324+Depreca1ed@users.noreply.github.com>
2026-02-01 17:33:56 +0530

    Re-format with vue official

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,17 @@
 <template>
   <div class="flex flex-col m-[5%] gap-y-10 justify-center items-center">
-    <div
-      class="flex md:flex-row flex-col flex-warp items-center justify-center gap-y-10 w-full"
-    >
-      <img
-        class="flex size-[50%] md:size-[25%] rounded-full outline-2 outline-offset-4 outline-[#cba6f7]/25"
-        :src="userdata.avatar.path"
-      />
+    <div class="flex md:flex-row flex-col flex-warp items-center justify-center gap-y-10 w-full">
+      <img class="flex size-[50%] md:size-[25%] rounded-full outline-2 outline-offset-4 outline-[#cba6f7]/25"
+        :src="userdata.avatar.path" />
       <div class="flex flex-col gap-y-5 items-center">
         <h1 class="font-bold text-5xl">{{ userdata.username }}</h1>
 
         <hr class="flex border border-[#cba6f7]/25 size-auto w-[75%]" />
 
-        <div
-          class="flex flex-row self-center items-center justify-center"
-          v-for="(i, social) in userdata.socials"
-        >
+        <div class="flex flex-row self-center items-center justify-center" v-for="(i, social) in userdata.socials">
           <a class="flex self-center items-center justify-center" :href="i">
             <span
-              class="flex items-center justify-center size-[10%] rounded-full hover:outline-2 outline-offset-4 outline-[#f38ba8]/50"
-            >
+              class="flex items-center justify-center size-[10%] rounded-full hover:outline-2 outline-offset-4 outline-[#f38ba8]/50">
               <img class="invert" :src="social" :href="i" />
             </span>
           </a>
@@ -36,14 +28,15 @@
     <div class="flex flex-col gap-y-5 w-[50%]">
       <h2 class="font-bold text-4xl mx-auto">Friends</h2>
       <div class="flex flex-col" v-for="friend in friends">
-        <div
-          class="flex items-center gap-5 rounded-full outline outline-offset-4 outline-[#f38ba8]/50"
-        >
+        <div class="flex items-center gap-5 rounded-full outline outline-offset-4 outline-[#f38ba8]/50">
           <img class="size-[15%] rounded-full" :src="friend.avatar" />
           <div class="my-max text-center">
             <p class="font-bold text-start text-2xl">
-            {{ friend.display_name ? friend.display_name : friend.name }}</p>
-              <p class="text-[#7f849c] font-light text-2xl" v-if=friend.display_name>{{ friend.name }}</p>
+              {{ friend.display_name ? friend.display_name : friend.name }}
+            </p>
+            <p class="text-[#7f849c] font-light text-2xl" v-if="friend.display_name">
+              {{ friend.name }}
+            </p>
           </div>
         </div>
       </div>
